7f57f397e02f085924205391368b7ef97e55a975
Anna <tomuchnure@gmail.com>
2024-09-27 12:41:17 +0700

    Добавьте файлы проекта.

---
+++ 2/2.cpp
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <conio.h>
+#include <iostream>
+
+int main()
+{
+	struct Image {};
+	struct Pixel {};
+	struct Filter {};
+	struct Layer {};
+	struct Pallete {};
+	struct File {};
+	struct ColorCorrection {};
+	struct BlendMode {};
+
+}
