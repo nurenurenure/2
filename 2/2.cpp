ee675c80ef3607808a7a3687c59b20ac05d434c5
Anna <tomuchnure@gmail.com>
2024-10-04 23:16:15 +0700

    Функции печати полей структур

--- 2/2.cpp
+++ 2/2.cpp
@@ -3,9 +3,12 @@
 #include <stdio.h>
 #include <conio.h>
 #include <iostream>
-void IncreaseProperty(float* property, float points) {
-	if (*property + points < 1) *property += points;
-	else *property = 1;
+void IncreaseProperty(float* property, float points, int code) {
+	if (code == 1) {
+		if (*property + points < 1) *property += points;
+		else *property = 1;
+	}
+	if (code == 0)*property += points;
 };
 
 void ChangeRGB(Pixel * object, int RGB[3]) {
@@ -16,11 +19,26 @@ void ChangeRGB(Pixel * object, int RGB[3]) {
 };
 
 void PixelInfoPrint(Pixel* object) {
-	printf("RED: %d, GREEN: %d, BLUE:%d, TRANSPERENCY:%f", object->R, object->G, object->B, object->transparency);
+	printf("RED: %d, GREEN: %d, BLUE:%d, transperency:%f", object->R, object->G, object->B, object->transparency);
 };
 
-void PixelSizeInfo(Image* object) {
-	printf("Width: %d, Height: %d, Pixel size: %d", object->width, object->height, (object->height)*(object->width));
+void ImageInfoPrint(Image* object) {
+	printf("Image name: %s, width: %d, height: %d", object->name, object->width, object->height);
+};
+
+void FilterInfoPrint(Filter* object) {
+	printf("Intensity:%f\n", object->intensity);
+	if (object->type == 0)printf("Filter type: NONE");
+	if (object->type == 1) printf("Filter type: Dark");
+
+};
+
+void LayerInfoPrint(Layer* object) {
+	printf("Opacity:%f, visible:%d, number:%d", object->opacity, object->visible, object->number);
+};
+
+void FileInfoPrint(File* object) {
+	printf("Name:%s, path: %s", object->name, object->path);
 };
 void PixelSet(Pixel* object) {
 	object->R = 0;
@@ -66,6 +84,4 @@ int main()
 	pixel->B = 15;
 	pixel->transparency = 0.4;
 	PixelInfoPrint(pixel);
-
-
 }
--- 2/myhead.h
+++ 2/myhead.h
@@ -2,7 +2,6 @@
 struct Image {
 	int width;
 	int height;
-	float color_depth;
 	char name[1000];
 };
 struct Pixel {
@@ -34,14 +33,19 @@ struct BlendMode {
 	float opacity;
 };
 
-void IncreaseProperty(float* property, float points);
+void IncreaseProperty(float* property, float points, int code);
 void ChangeRGB(Pixel* object, int RGB[3]);
-void PixelInfoPrint(Pixel* object);
-void PixelSizeInfo(Image* object);
 void PixelSet(Pixel* object);
 void FilterSet(Filter* object);
 void LayerSet(Layer* object);
 void FileSet(File* object);
 void ColorCorrectionSet(ColorCorrection* object);
 void BlendModeSet(BlendMode* object);
+
+void PixelInfoPrint(Pixel* object);
+void ImageInfoPrint(Image* object);
+void FilterInfoPrint(Filter* object);
+void LayerInfoPrint(Layer* object);
+void FileInfoPrint(File * object);
+
 
