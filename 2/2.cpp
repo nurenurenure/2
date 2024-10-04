aa697ed25687f5b550371a837e883216894b805d
Anna <tomuchnure@gmail.com>
2024-10-04 22:14:56 +0700

    Функции сеттеры

--- 2/2.cpp
+++ 2/2.cpp
@@ -3,41 +3,69 @@
 #include <stdio.h>
 #include <conio.h>
 #include <iostream>
-
-int pixel_size(int width, int height){
-	return width*height;
-}
-void increase_property(float* property, float points) {
+void IncreaseProperty(float* property, float points) {
 	if (*property + points < 1) *property += points;
 	else *property = 1;
-}
+};
 
-void change_name(char* name[], char new_name[]) {
-	strcpy(*name, new_name);
-};
-void change_RGB(int* R, int* G, int* B, int RGB[3]) {
-	*R = RGB[0];
-	*G = RGB[1];
-	*B = RGB[2];
-};
-void switch_layer_mode(bool* vision) {
-	if (*vision == true) *vision = false;
-	else *vision = true;
-};
-void increase_layer(int* number, int total_numb) {
-	if (*number < total_numb) *number += 1;
+void ChangeRGB(Pixel * object, int RGB[3]) {
+	object->R = RGB[0];
+	object->G = RGB[1];
+	object->B = RGB[2];
 
 };
-void decrease_layer(int* number, int total_numb) {
-	if (*number > 0) *number -= 1;
+
+void PixelInfoPrint(Pixel* object) {
+	printf("RED: %d, GREEN: %d, BLUE:%d, TRANSPERENCY:%f", object->R, object->G, object->B, object->transparency);
+};
+
+void PixelSizeInfo(Image* object) {
+	printf("Width: %d, Height: %d, Pixel size: %d", object->width, object->height, (object->height)*(object->width));
+};
+void PixelSet(Pixel* object) {
+	object->R = 0;
+	object->G = 0;
+	object->B = 0;
+	object->transparency = 1;
+};
+
+void FilterSet(Filter* object) {
+	object->type = 0;
+	object->intensity = 0;
+};
+
+void LayerSet(Layer* object) {
+	object->number = 0;
+	object->opacity = 1;
+	object->visible = 1;
+
+};
+
+void FileSet(File* object) {
+	strcpy(object->name, "");
+	strcpy(object->path, "");
+
+};
+
+void ColorCorrectionSet(ColorCorrection* object) {
+	object->brightness = 1;
+	object->contrast = 1;
+	object->saturation = 1;
+};
+void BlendModeSet(BlendMode* object) {
+	object->opacity = 1;
+	object->type = 0;
 };
 
 int main()
 {
-	ColorCorrection meow;
-	meow.brightness = 0.5;
-	increase_property(&meow.brightness, 0.4);
-	printf("Новое значение яркости: %f", meow.brightness);
+	Pixel* pixel;
+	pixel = (Pixel*)malloc(sizeof(Pixel));
+	pixel->R = 15;
+	pixel->G = 15;
+	pixel->B = 15;
+	pixel->transparency = 0.4;
+	PixelInfoPrint(pixel);
 
 
 }
--- 2/myhead.h
+++ 2/myhead.h
@@ -2,7 +2,7 @@
 struct Image {
 	int width;
 	int height;
-	int color_depth;
+	float color_depth;
 	char name[1000];
 };
 struct Pixel {
@@ -12,17 +12,13 @@ struct Pixel {
 	float transparency;
 };
 struct Filter {
-	char name[1000];
+	int type;
 	float intensity;
 };
 struct Layer {
 	float opacity;
 	bool visible;
 	int number;
-};
-struct Pallete {
-	int colors[1000][1000];
-
 };
 struct File {
 	char name[1000];
@@ -32,21 +28,20 @@ struct ColorCorrection {
 	float brightness;
 	float contrast;
 	float saturation;
-	float hue;
 };
 struct BlendMode {
-	char type[1000];
+	int type;
 	float opacity;
 };
 
-int pixel_size(int width, int height);
-void increase_property(float* property, float points);
-void change_name(char* name[], char new_name[]);
-void change_RGB(int* R, int* G, int* B, int RGB[3]);
-void switch_layer_mode(bool* vision);
-void increase_layer(int* number, int total_numb);
-void decrease_layer(int* number, int total_numb);
-
-
-
+void IncreaseProperty(float* property, float points);
+void ChangeRGB(Pixel* object, int RGB[3]);
+void PixelInfoPrint(Pixel* object);
+void PixelSizeInfo(Image* object);
+void PixelSet(Pixel* object);
+void FilterSet(Filter* object);
+void LayerSet(Layer* object);
+void FileSet(File* object);
+void ColorCorrectionSet(ColorCorrection* object);
+void BlendModeSet(BlendMode* object);
 
